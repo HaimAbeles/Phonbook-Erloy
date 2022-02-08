85306cca82843e9c018fc49808f3efb2bd8c6622
CHAIM-LAP\1 <a7112079@gmail.com>
2022-02-08 19:46:34 +0200

    Updates

---
+++ precache-manifest.f9dd19c1746beffe7a3d43040eafd302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7aa1a99f8fe0325aaa85144af23b726",
+    "url": "/Phonbook-Erloy/index.html"
+  },
+  {
+    "revision": "edea2de896f7b2dde37a",
+    "url": "/Phonbook-Erloy/static/css/main.1aedd73f.chunk.css"
+  },
+  {
+    "revision": "41d9b4c71c440c104d00",
+    "url": "/Phonbook-Erloy/static/js/2.f877b866.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/Phonbook-Erloy/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "edea2de896f7b2dde37a",
+    "url": "/Phonbook-Erloy/static/js/main.f708963c.chunk.js"
+  },
+  {
+    "revision": "87ef2b1999ed7b716576",
+    "url": "/Phonbook-Erloy/static/js/runtime~main.6bab7190.js"
+  },
+  {
+    "revision": "b111a28f23dfd666481ae8f879450ab3",
+    "url": "/Phonbook-Erloy/static/media/logo.b111a28f.png"
+  }
+]);
