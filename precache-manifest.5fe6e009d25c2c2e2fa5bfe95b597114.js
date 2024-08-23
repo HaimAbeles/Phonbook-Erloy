2feed3502a66e8f7808befaa7c3e48008b690896
HaimAbeles <a7112079@gmail.com>
2024-08-23 16:42:34 +0300

    Updates

---
+++ precache-manifest.5fe6e009d25c2c2e2fa5bfe95b597114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf088b0dacceb134f9e6a8275a532930",
+    "url": "/Phonbook-Erloy/index.html"
+  },
+  {
+    "revision": "8d552e7afd32d69435d0",
+    "url": "/Phonbook-Erloy/static/css/main.de47223e.chunk.css"
+  },
+  {
+    "revision": "2c65d733a8e1a0fe9d2c",
+    "url": "/Phonbook-Erloy/static/js/2.1cb0972c.chunk.js"
+  },
+  {
+    "revision": "61c75ad1c6ad002fdd6f",
+    "url": "/Phonbook-Erloy/static/js/3.a50d1155.chunk.js"
+  },
+  {
+    "revision": "8d552e7afd32d69435d0",
+    "url": "/Phonbook-Erloy/static/js/main.34217ec7.chunk.js"
+  },
+  {
+    "revision": "7011bd348d85872cf90c",
+    "url": "/Phonbook-Erloy/static/js/runtime~main.28bffc80.js"
+  },
+  {
+    "revision": "b111a28f23dfd666481ae8f879450ab3",
+    "url": "/Phonbook-Erloy/static/media/logo-big.b111a28f.png"
+  },
+  {
+    "revision": "50b8b069f52c060b4ec73e6712c4640e",
+    "url": "/Phonbook-Erloy/static/media/logo.50b8b069.png"
+  }
+]);
